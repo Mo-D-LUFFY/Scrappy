54859bd39fcc80cc0020c4873798018d4c6b3f53
Ayush Raj <parmod.k.201555@gmail.com>
2023-12-09 22:10:19 +0530

    Fixed Error

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,6 +5,10 @@ import { scrapeAmazonProduct } from "@/lib/scrapper";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300;
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export async function GET() {
     try {
         connectToDB();
@@ -29,7 +33,7 @@ export async function GET() {
             
     
                 const updatedProduct  = await Product.findOneAndUpdate(
-                { url: scrapedProduct.url },
+                { url: product.url },
                     product,
                 );
                 const emailNotifType = getEmailNotifType(scrapedProduct, currentProduct)
